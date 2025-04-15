b9acf86e3fe791682dcf4fcb6dca1d486789fc4b
MaskStarmoon <150095487+MaskStarmoon@users.noreply.github.com>
2025-04-15 03:57:12 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+/* Running the script with server, made by RangeStar */
+const { spawn } = require('child_process');
 
+function run() {
+  spawn("node server.js", {
+    cwd: __dirname,
+    stdio: "inherit",
+    shell: true
+  });
+
+  child.on("close", (code) => {
+    if (code == 2) {
+      run(); 
+    }
+  });
+}
+run();
+setInterval(function() {
+  console.clear();
+}, 3600000);
